b5ce53858a53b40fa95971f881c6547f7f892860
Jack Gutteridge <jack@kingbrick.co.uk>
2016-11-24 14:19:05 +0000

    Fixes Webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,7 @@ module.exports = {
     },
     plugins: [
         new webpack.DefinePlugin({
-            'process.env.NODE_ENV': process.env.NODE_ENV
+            'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
         }),
         new webpack.optimize.DedupePlugin(),
         new webpack.optimize.OccurenceOrderPlugin(),
