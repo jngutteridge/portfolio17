444b4d269177782e1b205a9dda32e5961ea8d00f
Jack Gutteridge <jack@kingbrick.co.uk>
2016-11-24 16:41:14 +0000

    Adds 404 support from server side statuses Minor refactor

--- src/server.jsx
+++ src/server.jsx
@@ -19,17 +19,17 @@ console.log(path.join(__dirname, 'web'));
 app.use('/', Express.static(path.join(__dirname, 'web')));
 
 // universal routing and rendering
-app.get('*', (req, res) => {
+app.get('*', (request, response) => {
     match(
-        { routes, location: req.url },
+        { routes, location: request.url },
         (error, redirectLocation, renderProps) => {
 
             if (error) {
-                return res.status(500).send(error.message);
+                return response.status(500).send(error.message);
             }
 
             if (redirectLocation) {
-                return res.redirect(302, redirectLocation.pathname + redirectLocation.search);
+                return response.redirect(302, redirectLocation.pathname + redirectLocation.search);
             }
 
             let main;
@@ -37,15 +37,16 @@ app.get('*', (req, res) => {
             if (renderProps) {
                 main = renderToString(<RouterContext {...renderProps}/>);
 
-                if (Number.isInteger(renderProps.status)) {
-                    res.status(renderProps.status)
+                // This is terrible, but can't find any other way
+                if (renderProps.components[1]().props.className === 'not-found') {
+                    response.status(404);
                 }
             } else {
                 main = renderToString(<NotFoundPage/>);
-                res.status(404);
+                response.status(404);
             }
 
-            return res.render('index', { main });
+            return response.render('index', { main });
         }
     );
 });
@@ -56,7 +57,7 @@ const port = process.env.PORT || 3000;
 const env = process.env.NODE_ENV || 'production';
 app.listen(port, error => {
     if (error) {
-        return console.erroror(error);
+        return console.error(error);
     }
     console.info(`Server running on http://localhost:${port} [${env}]`);
 });
